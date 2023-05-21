d3dd127cea45d0d5ffff322fd9444f71fb7135c3
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-05-21 16:21:28 +0200

    static middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const usersRouter = require('./router/users')
 
+app.use(express.static('public'))
 app.use('/users', usersRouter)
 app.set('view engine', 'ejs')
 app.listen(3000)
