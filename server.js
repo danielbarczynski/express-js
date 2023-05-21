4a5bcf5f918f1aaa5ff4f6d04db9d693707890b7
Daniel Barczyński <daniel.barczynski@onet.pl>
2023-05-21 12:27:54 +0200

    router

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
+const usersRouter = require('./router/users')
 
 app.set('view engine', 'ejs')
 app.listen(3000)
 app.get('/', (req, res) => {
-  // res.send('Hello world!')
-  // res.download('server.js')
   res.render('index', { text: 'bye'})
 })
+app.use('/users', usersRouter)
