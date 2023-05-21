9bdc7c6d967405acb45ca0bd6cf5a619caeba3ec
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-05-21 16:09:08 +0200

    route, param

--- router/users.js
+++ router/users.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const router = express.Router()
-
+const users = [{name: 'Daniel', age: 23}, {name: 'Mela'}
+]
 router.get('/', (req, res) => {
   res.send('Users')
 })
 
 router.get('/user', (req, res) => {
   res.send('User')
+})
+
+router.route('/:id').get((req, res) => {
+  console.log(req.user)
+  res.send(`Get user with id: ${req.params.id}`)
+}).post((req, res) => {
+  res.send(`Post user with id: ${req.params.id}`)
+}).delete((req, res) => {
+  res.send(`Delete user with id: ${req.params.id}`)
+})
+
+router.param('id', (req, res, next, id) => {
+  req.user = users[id]
+  next()
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const usersRouter = require('./router/users')
 
+app.use('/users', usersRouter)
 app.set('view engine', 'ejs')
 app.listen(3000)
 app.get('/', (req, res) => {
   res.render('index', { text: 'bye'})
 })
-app.use('/users', usersRouter)
